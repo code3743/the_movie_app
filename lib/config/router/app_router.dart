59623c359f46697788599b022802436091bb0e03
code3743 <jota.lopez@correounivalle.edu.co>
2024-10-19 13:59:06 -0500

    add routing for MovieDetails screen and update imports in app_router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart' show GlobalKey, NavigatorState;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:the_movie_app/ui/home/screen/home_screen.dart';
 import 'package:the_movie_app/config/router/app_router_name.dart';
-import 'package:the_movie_app/home_screen.dart';
-import 'package:the_movie_app/splash_screen.dart';
+import 'package:the_movie_app/ui/details/screen/movie_details.dart';
+import 'package:the_movie_app/ui/splash/screen/splash_screen.dart';
 
 final appRouterProvider = Provider((ref) => GoRouter(
         navigatorKey: GlobalKey<NavigatorState>(),
@@ -14,7 +15,12 @@ final appRouterProvider = Provider((ref) => GoRouter(
             builder: (context, state) => const SplashScreen(),
           ),
           GoRoute(
-            path: AppRouterName.home,
-            builder: (context, state) => const HomeScreen(),
-          ),
+              path: AppRouterName.home,
+              builder: (context, state) => const HomeScreen(),
+              routes: [
+                GoRoute(
+                  path: AppRouterName.movieDetail,
+                  builder: (context, state) => const MovieDetails(),
+                )
+              ]),
         ]));
