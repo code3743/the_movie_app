3d46c6274920ccb0c6d58283666f4834ae856acf
code3743 <jota.lopez@correounivalle.edu.co>
2024-10-19 13:58:57 -0500

    update secondary color and add accentDark color to AppColors

--- lib/config/themes/app_colors.dart
+++ lib/config/themes/app_colors.dart
@@ -4,9 +4,10 @@ class AppColors {
   AppColors._();
 
   static const Color primary = Color(0xFFd12f26);
-  static const Color secondary = Color(0xFF181818);
+  static const Color secondary = Color(0xFF393939);
   static const Color background = Color(0xFF141414);
   static const Color accent = Color(0xFFFFFFFF);
+  static const Color accentDark = Color(0xFF181818);
   static const Color textPrimary = Color(0xFFFFFFFF);
   static const Color textSecondary = Color(0xFF515151);
   static const Color error = Color(0xFFd12f26);
